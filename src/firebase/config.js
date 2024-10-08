698e740b56bbd0264df3ccfba2ddd1fa19d6a6de
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2024-10-08 18:51:52 +0900

    Basic Page Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCt6ckMfgFYhv3wiwK2Lrmz69DIyLWcm-0',
+  authDomain: 'bakery-page.firebaseapp.com',
+  projectId: 'bakery-page',
+  storageBucket: 'bakery-page.appspot.com',
+  messagingSenderId: '766885840314',
+  appId: '1:766885840314:web:07ddde55374afac1560983',
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init firestore service
+const projectFirestore = firebase.firestore();
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, timestamp };
