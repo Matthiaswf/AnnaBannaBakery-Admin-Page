7077ac728c150fea201ca7a30fe0ecc9ef601690
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2024-10-30 22:37:44 +0900

    Loading Orders Message

--- src/views/OrderListView.vue
+++ src/views/OrderListView.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Orders</h1>
+    <div class="pending-text" v-if="isPending">Loading Orders...</div>
     <div v-if="orders.length === 0" class="no-orders-text">
       No current Orders.
     </div>
@@ -14,14 +15,21 @@
 import getCollection from '@/utils/getCollection';
 import OrderDetails from '@/components/OrderDetails.vue';
 import { computed } from 'vue';
+import { ref } from 'vue';
+import { watchEffect } from 'vue';
 
 export default {
   components: {
     OrderDetails,
   },
   setup() {
+    const isPending = ref(false);
     const { documents: orders } = getCollection('orders');
 
+    // Watch for changes in the orders collection
+    watchEffect(() => {
+      isPending.value = !orders.value;
+    });
     const sortedOrders = computed(() => {
       return orders.value
         ? orders.value.slice().sort((a, b) => b.createdAt - a.createdAt)
@@ -29,6 +37,7 @@ export default {
     });
 
     return {
+      isPending,
       orders: sortedOrders,
       OrderDetails,
     };
@@ -47,6 +56,11 @@ export default {
 }
 h1 {
   padding: 0;
+}
+.pending-text {
+  font-size: 2rem;
+  margin-top: 20px;
+  padding-left: 5px;
 }
 .no-orders-text {
   font-size: 20px;
