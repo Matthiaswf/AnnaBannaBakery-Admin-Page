bf51b6a5a625794d2f617ce85cbdf092625f268b
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2024-10-31 19:18:34 +0900

    Displays Total Ingredients added togethers

--- src/components/ShoppingList.vue
+++ src/components/ShoppingList.vue
@@ -1,13 +1,9 @@
 <template>
   <div class="shopping-list-container">
     <h2>Shopping List</h2>
-    <div
-      v-if="order && order.cart"
-      v-for="product in order.cart"
-      :key="product.id"
-    >
-      <div v-for="ingredient in product.ingredients" :key="ingredient.id">
-        {{ ingredient.name }}: {{ ingredient.quantity * product.quantity }}
+    <div v-if="order && order.cart">
+      <div v-for="(quantity, name) in aggregatedIngredients" :key="name">
+        {{ name }}: {{ quantity }}
       </div>
     </div>
   </div>
@@ -16,7 +12,27 @@
 <script>
 export default {
   props: ['order'],
+  computed: {
+    aggregatedIngredients() {
+      const ingredientsMap = {};
+      if (this.order && this.order.cart) {
+        this.order.cart.forEach((product) => {
+          product.ingredients.forEach((ingredient) => {
+            const totalQuantity = ingredient.quantity * product.quantity;
+            if (ingredientsMap[ingredient.name]) {
+              ingredientsMap[ingredient.name] += totalQuantity;
+            } else {
+              ingredientsMap[ingredient.name] = totalQuantity;
+            }
+          });
+        });
+      }
+      return ingredientsMap;
+    },
+  },
 };
 </script>
+
+<style scoped></style>
 
 <style scoped></style>
