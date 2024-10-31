fbebd331647fa55f4c339e75c36e5e5767e35bb6
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2024-10-31 17:46:45 +0900

    Orders Filter Implemented

--- src/views/OrderListView.vue
+++ src/views/OrderListView.vue
@@ -1,12 +1,43 @@
 <template>
   <div>
     <h1>Orders</h1>
+
+    <!-- Filter Nav -->
+    <nav class="filter">
+      <button @click="filter = 'all'" :class="{ active: filter === 'all' }">
+        All
+      </button>
+      <button
+        @click="filter = 'Pending'"
+        :class="{ active: filter === 'Pending' }"
+      >
+        Pending
+      </button>
+      <button
+        @click="filter = 'Complete'"
+        :class="{ active: filter === 'Complete' }"
+      >
+        Complete
+      </button>
+    </nav>
+
     <div class="pending-text" v-if="isPending">Loading Orders...</div>
     <div v-if="!isPending && sortedOrders.length === 0" class="no-orders-text">
       No current Orders.
     </div>
-    <div v-for="item in sortedOrders" :key="item.id" class="order-container">
-      <OrderDetails :order="item" />
+    <div v-if="filter === 'all'">
+      <div v-for="item in sortedOrders" :key="item.id">
+        <OrderDetails v-if="item" :order="item" class="order-container" />
+      </div>
+    </div>
+    <div v-else>
+      <div v-for="item in sortedOrders" :key="item.id">
+        <OrderDetails
+          v-if="item && item.status === filter"
+          :order="item"
+          class="order-container"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -26,6 +57,8 @@ export default {
     const isPending = ref(false);
     const { documents: orders } = getCollection('orders');
 
+    const filter = ref('all');
+
     // Watch for changes in the orders collection
     watchEffect(() => {
       isPending.value = !orders.value;
@@ -39,6 +72,7 @@ export default {
     return {
       isPending,
       sortedOrders,
+      filter,
       OrderDetails,
     };
   },
@@ -66,5 +100,38 @@ h1 {
   font-size: 20px;
   margin-top: 20px;
   padding-left: 5px;
+}
+
+.filter {
+  display: flex;
+  justify-content: center;
+  margin: 20px 0;
+  margin-bottom: 60px;
+}
+
+.filter button {
+  min-width: 130px;
+  height: 40px;
+  color: var(--primary);
+  padding: 5px 10px;
+  font-weight: bold;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  position: relative;
+  display: inline-block;
+  outline: none;
+  border-radius: 5px;
+  border: 2px solid var(--primary);
+  background: var(--background);
+  margin: 0 5px;
+}
+
+.filter button:hover {
+  background-color: var(--background);
+  color: var(--primary);
+}
+.filter button.active {
+  background-color: var(--background);
+  color: white;
 }
 </style>
